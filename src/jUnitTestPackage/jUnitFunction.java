0f90702182ff140b7f1c2b0c62fa705e370f0407
ABHINAV CHANGIL <ABHINAV CHANGIL@10.12.17.93>
2022-11-21 10:08:48 +0530

    jUnitTest

---
+++ src/jUnitTestPackage/jUnitFunction.java
@@ -0,0 +1,19 @@
+package jUnitTestPackage;
+
+public class jUnitFunction {
+	
+	public static int addNumbers(int n1, int n2) {
+		int sum = n1+n2;
+		return sum;
+	}
+	
+	public static String addString(String s1, String s2) {
+		String str = s1+s2;
+		return str;
+	}
+	
+	public static void main(String args[]){
+		
+	}
+
+}
